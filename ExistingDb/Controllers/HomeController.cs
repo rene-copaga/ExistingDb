403e9110e596172883a84193f5ce29314aa98bb8
Rene Copaga <rene.copaga@gmail.com>
2020-05-10 20:46:14 -0400

    Creating the ASP.NET Core MVC Project

---
+++ ExistingDb/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExistingDb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
