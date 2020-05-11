6ac896f35d0844146a6987825979168b2121c96e
Rene Copaga <rene.copaga@gmail.com>
2020-05-10 22:33:13 -0400

    Using a Scaffolded Data Model in the ASP.NET Core MVC

--- ExistingDb/Controllers/HomeController.cs
+++ ExistingDb/Controllers/HomeController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExistingDb.Models.Scaffold;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExistingDb.Controllers
 {
     public class HomeController : Controller
     {
+        private ScaffoldContext context;
+
+        public HomeController(ScaffoldContext ctx) => context = ctx;
+
         public IActionResult Index()
         {
-            return View();
+            return View(context.Shoes
+                .Include(s => s.Color)
+                .Include(s => s.SalesCampaigns)
+                .Include(s => s.ShoeCategoryJunction)
+                    .ThenInclude(junct => junct.Category));
         }
     }
 }
