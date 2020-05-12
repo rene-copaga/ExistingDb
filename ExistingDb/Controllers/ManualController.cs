211b9a25e50a9dfac517134c9f55c4875e133661
Rene Copaga <rene.copaga@gmail.com>
2020-05-12 16:57:11 -0400

    Updating Data in a Manually Created Data Model

--- ExistingDb/Controllers/ManualController.cs
+++ ExistingDb/Controllers/ManualController.cs
@@ -1,6 +1,8 @@
 ﻿using ExistingDb.Models.Manual;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ExistingDb.Controllers
 {
@@ -19,6 +21,40 @@ namespace ExistingDb.Controllers
             return View(context.Shoes.Include(s => s.Style)
                 .Include(s => s.Width).Include(s => s.Categories)
                     .ThenInclude(j => j.Category));
+        }
+
+        public IActionResult Edit(long id)
+        {
+            ViewBag.Styles = context.ShoeStyles;
+            ViewBag.Widths = context.ShoeWidths;
+            ViewBag.Categories = context.Categories;
+            return View(context.Shoes.Include(s => s.Style)
+                .Include(s => s.Campaign)
+                .Include(s => s.Width).Include(s => s.Categories)
+                .ThenInclude(j => j.Category).First(s => s.Id == id));
+        }
+
+        [HttpPost]
+        public IActionResult Update(Shoe shoe, long[] newCategoryIds, 
+                ShoeCategoryJunction[] oldJunctions)
+        {
+            IEnumerable<ShoeCategoryJunction> unchangedJunctions
+                = oldJunctions.Where(j => newCategoryIds.Contains(j.CategoryId));
+
+            context.Set<ShoeCategoryJunction>()
+                .RemoveRange(oldJunctions.Except(unchangedJunctions));
+
+            shoe.Categories = newCategoryIds.Except(unchangedJunctions
+                .Select(j => j.CategoryId))
+                .Select(id => new ShoeCategoryJunction
+                {
+                    ShoeId = shoe.Id,
+                    CategoryId = id
+                }).ToList();
+
+            context.Shoes.Update(shoe);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
